dfa004b5cd62fec8a8aff9e246382c210062bd79
underwoodjo <132414633+underwoodjo@users.noreply.github.com>
2024-05-01 09:56:23 -0400

    added files and folders, started to design landing

---
+++ login.js
@@ -0,0 +1,7 @@
+const loginButton = document.querySelector('#loginButton');
+
+function loginPage() {
+console.log('Login to account');
+}
+// used the const that has the element #login-button and set it to the function loginPage
+loginButton.onclick = loginPage;
